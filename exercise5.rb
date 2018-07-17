5396fb849b9f83e3a88c7000e125670c407ea1b6
schwarro <rob.ap.schwartz@gmail.com>
2018-07-17 15:03:03 -0400

    completed exercise 5

---
+++ exercise5.rb
@@ -0,0 +1,9 @@
+def greet_backwards(name)
+  rev = name.reverse
+  puts "Hello, #{rev}! Welcome home."
+end
+
+puts greet_backwards("Bob")
+puts greet_backwards("Shirly")
+puts greet_backwards("Sue")
+puts greet_backwards("Andy")
